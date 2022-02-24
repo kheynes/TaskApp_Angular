6c2cf16a9d98c298d4ae6f35af469278147d395a
Kheynes <heyneskeziah@gmail.com>
2022-02-24 15:49:46 +0200

    added a service for tasks

--- task-app/src/app/components/tasks/tasks.component.ts
+++ task-app/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../services/task.service';
+
 
 @Component({
 	selector: 'app-tasks',
@@ -10,11 +11,12 @@ import { TASKS } from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-	tasks: Task[] = TASKS;
+	tasks: Task[] = [];
 
-	constructor() { }
+	constructor(private taskService: TaskService) { }
 
 	ngOnInit(): void {
+		this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
 	}
 
 }
