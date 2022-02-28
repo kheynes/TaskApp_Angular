209203c21358809c9f84fe750dc17d513a1b4173
Kheynes <heyneskeziah@gmail.com>
2022-02-28 15:11:34 +0200

    Added reminder functionality

--- task-app/src/app/components/task-item/task-item.component.ts
+++ task-app/src/app/components/task-item/task-item.component.ts
@@ -3,24 +3,26 @@ import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-	selector: 'app-task-item',
-	templateUrl: './task-item.component.html',
-	styleUrls: ['./task-item.component.css']
+    selector: 'app-task-item',
+    templateUrl: './task-item.component.html',
+    styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
-	@Input() task!: Task;
-	@Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+    @Input() task!: Task;
+    @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+    @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
 
-	faTimes = faTimes;
+    faTimes = faTimes;
 
-	constructor() { }
+    constructor() { }
 
-	ngOnInit(): void {
-	}
+    ngOnInit(): void { }
 
-	onDelete(task: Task) {
-		this.onDeleteTask.emit(task);
-		
-	}
+    onDelete(task: Task) {
+        this.onDeleteTask.emit(task);
+    }
 
+    onToggle(task: Task){
+        this.onToggleReminder.emit(task);
+    }
 }
